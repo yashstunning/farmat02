5442b019a9027e879eba11b01e98817eb1af80af
Yash sharma <144417324+yashstunning@users.noreply.github.com>
2024-10-12 08:26:20 +0000

    config done

--- .devcontainer/basics/variable.js
+++ .devcontainer/basics/variable.js
@@ -1,1 +1,10 @@
-console.log("Yash sharma");
+const account_Id = 11530
+let account_Name = "Yash"
+var acocunt_email = "yahs@google.acocunt"
+account_city = "Karnal"
+let acocunt_State;
+
+
+// console.log(account_Id);
+
+console.table([ account_Id,account_Name,acocunt_email,account_city,acocunt_State])
